5b32b1754600187e8e4779cedec8d0a2541d4851
Christopher D. Paucar <cpaucar@armlab01.cs.princeton.edu>
2020-11-30 19:23:36 -0500

    start assembler

--- miniassembler.c
+++ miniassembler.c
@@ -32,3 +32,38 @@ unsigned int MiniAssembler_adr(unsigned int uiReg, unsigned long ulAddr,
    
    return uiInstr;
 }
+
+unsigned int MiniAssembler_mov(unsigned int uiReg, unsigned int uiImmed)
+{
+   unsigned int uiInstr;
+
+   /*instruction before implementing uiReg, uiImmed*/
+   uiInstr=0x5200000;
+   uiInstr |= uiReg;
+
+   return uiInstr;
+}
+
+unsigned int MiniAssembler_strb(unsigned int uiFromReg,
+                                unsigned int uiToReg)
+{
+   unsigned int uiInstr;
+
+   return uiInstr;
+}
+
+unsigned int MiniAssembler_b(unsigned long ulAddr,
+                             unsigned long ulAddrOfThisInstr)
+{
+   unsigned int uiInstr;
+   unsigned int offset;
+
+   /*instruction before implementing params*/
+   uiInstr=0x10000000;
+   /*calculate and apply offset*/
+   offset=ulAddr-ulAddrOfThisInstr;
+   offset=offset >> 2;
+   offset |= 
+   
+   return uiInstr;
+}
