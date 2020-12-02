9c0856e4ac5186313f73551e4174961fad381faa
Christopher D. Paucar <cpaucar@armlab02.cs.princeton.edu>
2020-12-01 21:13:41 -0500

    mov done, b maybe

--- miniassembler.c
+++ miniassembler.c
@@ -36,10 +36,13 @@ unsigned int MiniAssembler_adr(unsigned int uiReg, unsigned long ulAddr,
 unsigned int MiniAssembler_mov(unsigned int uiReg, unsigned int uiImmed)
 {
    unsigned int uiInstr;
-
    /*instruction before implementing uiReg, uiImmed*/
-   uiInstr=0x5200000;
-   uiInstr |= uiReg;
+   uiInstr=0x5280000;
+   uiInstr |= uiReg;/*instruction*/
+
+   uiImmed <<= 5;/*shift left by 5 to get to correct location*/
+   uiImmed &= 0x001fffe0;/*mask by using &=*/
+   uiInstr |= uiImmed;/*or this to the instruction*/
 
    return uiInstr;
 }
@@ -59,11 +62,17 @@ unsigned int MiniAssembler_b(unsigned long ulAddr,
    unsigned int offset;
 
    /*instruction before implementing params*/
-   uiInstr=0x10000000;
+   uiInstr=0x14000000;
    /*calculate and apply offset*/
    offset=ulAddr-ulAddrOfThisInstr;
+   /*get rid of the last two bits*/
    offset=offset >> 2;
-   offset |= 
-   
+   /*mask*/
+   /*0001 0011 1111 11111 1111 1111 1111 1111 */
+   /*1     3   f    f      f    f    f     f*/
+   offset &= 0x03ffffff;  
+   /*or offset with instruction*/
+
+   uiInstr |= offset;
    return uiInstr;
 }
